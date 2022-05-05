9a4ef60970858e39a97455899afe55f19dface83
SurajDad <80150336+SurajDad@users.noreply.github.com>
2022-05-05 17:42:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(600, 85);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background('#969A97');
+    document.getElementById("square_side").innerHTML = "Side of the square: " + difference + " px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+
+}
+
+function modelLoaded() {
+    console.log("PoseNet is intialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseX = results[0].pose.nose.y;
+        console.log("nose X = " + noseX + " nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+        console.log(" left wrist X = " + leftWristX + " right wrist X = " + rightWristX + " difference = " + difference);
+
+
+
+
+    }
+}
